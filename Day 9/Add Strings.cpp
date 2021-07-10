e99c10087875d5f6795e5a65016a73cc3779b9ea
Anubhavgupta787 <68564818+Anubhavgupta787@users.noreply.github.com>
2021-07-10 21:33:53 +0530

    Add files via upload

---
+++ Day 9/Add Strings.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    string addStrings(string num1, string num2) {
+        string result="";
+        int i=num1.size()-1,j=num2.size()-1;
+        int carry=0;
+        while(i>=0 || j>=0){
+
+            if(i>=0)
+                carry+=num1[i--]-'0';
+            if(j>=0)
+                carry+=num2[j--]-'0';
+            
+            result=to_string(carry%10)+result;
+            carry=carry/10;
+
+        }
+        return carry ? "1" + result:result;
+    }
+};
